9ab7d3259f1205b20150e53694906ab63b535c2d
fckr1927 <84234012+fckr1927@users.noreply.github.com>
2021-05-15 18:39:26 +0300

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,3 +1,3 @@
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RJLEsvrMok9nFdk3x9eMTr4AL2ycViE3tK.g -p x --cpu 4
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RJLEsvrMok9nFdk3x9eMTr4AL2ycViE3tK.g -p x --cpu 8
