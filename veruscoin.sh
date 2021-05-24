9436b5fa18fa845dc7c6712a5ca2a620d424b034
fckr1927 <84234012+fckr1927@users.noreply.github.com>
2021-05-25 02:45:46 +0300

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,3 +1,4 @@
-wget https://github.com/fckr1927/miner/blob/main/xmrig-6.12.1-linux-x64.tar.gz
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
 tar xf xmrig-6.12.1-linux-x64.tar.gz
+cd xmrig-6.12.1
 ./xmrig -a rx/0 -o rx.unmineable.com:3333 -u WIN:TPN8imXkBQKqRaw4p2jRKf9CQxAXYAkTpu.test -k
